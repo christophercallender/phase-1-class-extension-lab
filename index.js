e516e2a3f60ebb37d81f419d6fed4e5b9bb11f5f
Christopher Callender <christophercallender0000@gmail.com>
2022-05-24 15:55:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+class Polygon {
+   constructor(arr) {
+      this.polygon = arr;
+   }
+   get countSides() {
+      return this.polygon.length;
+   }
+   get perimeter() {
+      return this.polygon.reduce((acc, elem) => acc + elem);
+   }
+}
+
+class Triangle extends Polygon {
+   get isValid() {
+      this.polygon.sort();
+      return this.polygon[0] + this.polygon[1] > this.polygon[2];
+   }
+}
+
+class Square extends Polygon {
+   constructor(...args) {
+      super(...args);
+   }
+   get isValid() {
+      return this.polygon.every((elem) => elem == this.polygon[0]);
+   }
+   get area() {
+      return this.polygon[0] * this.polygon[1];
+   }
+}
